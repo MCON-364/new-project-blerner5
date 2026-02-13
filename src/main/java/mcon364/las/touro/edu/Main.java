749a1a51d2396016a4f3b65796090016f52eb9b0
Your NameBrooke <blerner5@student.touro.edu>
2026-02-12 23:39:07 -0500

    Submit assignment

--- src/main/java/mcon364/las/touro/edu/Main.java
+++ src/main/java/mcon364/las/touro/edu/Main.java
@@ -5,7 +5,9 @@ import java.util.List;
 import java.util.Optional;
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(getGreeting("USERNAME"));
+        System.out.println(getGreeting("NO_SUCH_VAR"));
+        processValues( List.of( List.of(1, 2, 3), List.of(0, 9, 8), List.of(4, 5, 99), List.of(7, 8, 9) ) );
     }
 
     public static Optional<String> getUserName(String name) {
@@ -22,8 +24,6 @@ public class Main {
         greeting.append("Hello, " + greetingName.get());
         return greeting.toString();
     }
-}
-
 public static void processValues(List<List<Integer>> data){
         List<List<Integer>> guests = new ArrayList<>();
         for (int i = 1; i <= guests.size(); i++) {
